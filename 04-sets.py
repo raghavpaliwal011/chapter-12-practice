c5f92ebcc74945461edfae66a86221f9fc5317e6
raghav <raghavpaliwal1109@gmail.com>
2024-05-05 17:08:54 +0530

    revision

---
+++ 04-sets.py
@@ -0,0 +1,4 @@
+my_set={1,2,3,4,5}
+my_set.add(6)#this add function will add 6
+my_set.remove(1)#this remove function will remove 1
+print (my_set)
---
+++ dictionary.py
@@ -0,0 +1,4 @@
+my_dict={"name":"raghav","age":13,"country":"india"}
+print(my_dict["name"])
+my_dict["age"] = 14
+print (my_dict)
